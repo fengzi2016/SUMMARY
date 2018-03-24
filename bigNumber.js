cb06c8fa06f7e0d14f9e12fe5dc08caa4e059552
fengzi2016 <1820166258@qq.com>
2018-03-24 23:30:23 +0800

    bigNumber

---
+++ bigNumber.js
@@ -0,0 +1,28 @@
+// JavaScript实现大数相加
+//思路：利用字符串数字，从低位向高位加
+
+function BigNumber(a,b){
+    a = a.toString();
+    b = b.toString();
+    let overNum = 0;
+    let sum,inNum;
+    let arrA = a.split('').reverse();
+    let arrB = b.split('').reverse();
+    let resultArr = [];
+    let len = Math.max(arrA.length,arrB.length);
+    for(let i = 0;i<len;i++){
+        sum = overNum;
+        if(i<arrA.length){
+            sum =  sum + arrA[i];
+        }
+        if(i<arrB.length){
+            sum = sum+ arrB[i];
+        }
+        inNum = sum % 10;
+        resultArr.push(inNum);
+        overNum = Math.floor(sum / 10) ;
+        
+    }
+    return resultArr.reverse().join("");
+
+}
