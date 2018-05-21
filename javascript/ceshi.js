b1eef1954f74e7a5a867a1d4b78b889d64f7163d
fengzi2016 <1820166258@qq.com>
2018-05-21 21:39:41 +0800

    feat: add zhengze and date learn

--- javascript/ceshi.js
+++ javascript/ceshi.js
@@ -1,4 +1,58 @@
 
-console.log(([])?true:false); 
-console.log(([]==false?true:false)); 
-console.log(({}==false)?true:false) 
+// console.log(([])?true:false); 
+// console.log(([]==false?true:false)); 
+// console.log(({}==false)?true:false) 
+// 'use strict'
+
+// const where = () => {
+//    //获取调用函数名字
+//    //不严格模式下可以 arguments.caller.name;
+//    let reg = /\s+at\s(\S+)\s\(/g;
+//    let str = new Error().stack.toString()
+//    let res = reg.exec(str) && reg.exec(str)
+//    return res[0]&&res[1];
+// }
+// var myRe = /d(b+)d/g;
+// var str = "cdbbdbsbz,cdbbdbsbz";
+// var myArray = myRe.exec(str);
+// var b = myRe.test(str);
+// var m = str.match(myRe);
+// var s = str.search(myRe);
+// var r = str.replace(myRe,'$1')
+// var sp = str.split(myRe);
+// console.log('a:'+myArray[0])
+// console.log('b:'+b);
+// console.log('m:'+m);
+// console.log('s:'+s);
+// console.log('r:'+r);
+// console.log('sp:'+sp)
+// //分解路由
+// const parseQueryString = function (query) {
+    
+//    // let myRe = /\?\w*(=\w*)*((&\w+){1}(=\w(?!#)*)*)*/g;
+//     let myRe = /\?\S*(=\w*)*((&\w+){1}(=\w(?!#)*)*)*/g;
+//     let result = {};
+//     let r = myRe.exec(query);
+//     if(r!==null){
+//         let myArr =r[0] ;
+//         let qu = myArr.split('?')[1];
+//         qu = qu.split('&');
+//         qu.map((item)=>{
+//            let arr = item.split('=');
+//            if(arr.length==2)
+//             result[arr[0]] = arr[1] || '';
+//            else result[arr[0]] = null;
+//         })
+//     }
+//     return result;
+    
+//   }
+//   console.log(parseQueryString('  https://scriptoj.com/problems/?offset=&limit=100#name=jerry '));
+//   //console.log(parseQueryString('https://scriptoj.com/problems/#?offset=10&limit=100'))
+
+let  time = '2014-04-23'
+let date  = new Date(time);
+console.log(Date.parse(time))
+console.log(Date.parse(date))
+console.log(date.valueOf())
+console.log(date.getTime())
--- self/toutiao_interview.md
+++ self/toutiao_interview.md
@@ -8,3 +8,23 @@
 4. bind,this
 5. 原码补码反码
 6. 实现array
+7. 保证ajax拉取的数据在用户点击按钮前能获取到
+
+### 1. date时间戳
+1. 日期转化为时间戳
+```js
+    let  time = '2014-04-23 18:55:49:123'
+    let date  = new Date(time);
+    console.log(Date.parse(time))
+    console.log(Date.parse(date))
+    console.log(date.valueOf())
+    console.log(date.getTime())
+```
+特点：  
+1. 必须将日期字符串转化成Date实例才能调用valueOf()，getTime()方法
+2. Date.parse()方法可以传字符串也可以传Date实例
+### 3.学习正则表达式
+
+
+刷题练习
+
