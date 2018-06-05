523a783ea4672a978e9ed62e2ca9076136b9839c
fengzi2016 <1820166258@qq.com>
2018-06-05 20:18:47 +0800

    js scope

--- interview/youzan_interview.md
+++ interview/youzan_interview.md
@@ -3,7 +3,7 @@
 ## 没有回答好的题目：
 
 
-1. js 作用域
+1. JavaSript 作用域
 2. MYSQL 索引
 3. 修改this的方法
 4. https 密钥
@@ -12,9 +12,36 @@
 7. koa和express 的区别，koa轻量级在哪里
 
 ## JS基础篇
-[you don't know javascript](https://github.com/getify/You-Dont-Know-JS/blob/master/up%20%26%20going/ch1.md)
-[eloquent javascript](https://eloquentjavascript.net/)
-[javacript ninja](https://www.manning.com/books/secrets-of-the-javascript-ninja)
+- [you don't know javascript](https://github.com/getify/You-Dont-Know-JS/blob/master/up%20%26%20going/ch1.md)
+
+- [eloquent javascript](https://eloquentjavascript.net/)
+
+- [javacript ninja](https://www.manning.com/books/secrets-of-the-javascript-ninja)
 
 
 1. 作用域
+
+- JavaScirpt 有 **全局作用域，函数作用域，eval作用域**，**没有块作用域**，
+
+- 函数作用域：一个函数中的任何位置定义的变量在该函数中的任何地方都是可见的。
+
+- 块作用域：任何一对花括号中的语句集都属于一个块，在这之中定义的所有变量在代码块外都是不可见的，我们称之为块级作用域。
+
+- 如何让JS模拟出块级作用域：
+
+    把语句块放到一个闭包之中，然后调用这个函数，当函数调用完毕，变量自动销毁，相当于给这个闭包加了一个函数作用域，因此，我们在块外便无法访问了。
+    例子：
+```js
+    
+    function grid(){
+        (function() {
+           var i = 1;
+        })();
+
+        console.log(i);
+        //ReferenceError: i is not defined
+    }
+    grid();
+    //弹出i未定义的错误
+```
+-  JavaScript 采用的是词法作用域即静态作用域，函数的作用域在函数定义的时候就决定了。而动态作用域的意思是，函数作用域在执行的时候才决定。
--- javascript/ceshi.js
+++ javascript/ceshi.js
@@ -1,66 +1,66 @@
 
-// // // console.log(([])?true:false); 
-// // // console.log(([]==false?true:false)); 
-// // // console.log(({}==false)?true:false) 
-// // // 'use strict'
+// console.log(([])?true:false); 
+// console.log(([]==false?true:false)); 
+// console.log(({}==false)?true:false) 
+// 'use strict'
 
-// // // const where = () => {
-// // //    //获取调用函数名字
-// // //    //不严格模式下可以 arguments.caller.name;
-// // //    let reg = /\s+at\s(\S+)\s\(/g;
-// // //    let str = new Error().stack.toString()
-// // //    let res = reg.exec(str) && reg.exec(str)
-// // //    return res[0]&&res[1];
-// // // }
-// // // var myRe = /d(b+)d/g;
-// // // var str = "cdbbdbsbz,cdbbdbsbz";
-// // // var myArray = myRe.exec(str);
-// // // var b = myRe.test(str);
-// // // var m = str.match(myRe);
-// // // var s = str.search(myRe);
-// // // var r = str.replace(myRe,'$1')
-// // // var sp = str.split(myRe);
-// // // console.log('a:'+myArray[0])
-// // // console.log('b:'+b);
-// // // console.log('m:'+m);
-// // // console.log('s:'+s);
-// // // console.log('r:'+r);
-// // // console.log('sp:'+sp)
-// // // //分解路由
-// // // const parseQueryString = function (query) {
+// const where = () => {
+//    //获取调用函数名字
+//    //不严格模式下可以 arguments.caller.name;
+//    let reg = /\s+at\s(\S+)\s\(/g;
+//    let str = new Error().stack.toString()
+//    let res = reg.exec(str) && reg.exec(str)
+//    return res[0]&&res[1];
+// }
+// var myRe = /d(b+)d/g;
+// var str = "cdbbdbsbz,cdbbdbsbz";
+// var myArray = myRe.exec(str);
+// var b = myRe.test(str);
+// var m = str.match(myRe);
+// var s = str.search(myRe);
+// var r = str.replace(myRe,'$1')
+// var sp = str.split(myRe);
+// console.log('a:'+myArray[0])
+// console.log('b:'+b);
+// console.log('m:'+m);
+// console.log('s:'+s);
+// console.log('r:'+r);
+// console.log('sp:'+sp)
+// //分解路由
+// const parseQueryString = function (query) {
     
-// // //    // let myRe = /\?\w*(=\w*)*((&\w+){1}(=\w(?!#)*)*)*/g;
-// // //     let myRe = /\?\S*(=\w*)*((&\w+){1}(=\w(?!#)*)*)*/g;
-// // //     let result = {};
-// // //     let r = myRe.exec(query);
-// // //     if(r!==null){
-// // //         let myArr =r[0] ;
-// // //         let qu = myArr.split('?')[1];
-// // //         qu = qu.split('&');
-// // //         qu.map((item)=>{
-// // //            let arr = item.split('=');
-// // //            if(arr.length==2)
-// // //             result[arr[0]] = arr[1] || '';
-// // //            else result[arr[0]] = null;
-// // //         })
-// // //     }
-// // //     return result;
+//    // let myRe = /\?\w*(=\w*)*((&\w+){1}(=\w(?!#)*)*)*/g;
+//     let myRe = /\?\S*(=\w*)*((&\w+){1}(=\w(?!#)*)*)*/g;
+//     let result = {};
+//     let r = myRe.exec(query);
+//     if(r!==null){
+//         let myArr =r[0] ;
+//         let qu = myArr.split('?')[1];
+//         qu = qu.split('&');
+//         qu.map((item)=>{
+//            let arr = item.split('=');
+//            if(arr.length==2)
+//             result[arr[0]] = arr[1] || '';
+//            else result[arr[0]] = null;
+//         })
+//     }
+//     return result;
     
-// // //   }
-// // //   console.log(parseQueryString('  https://scriptoj.com/problems/?offset=&limit=100#name=jerry '));
-// // //   //console.log(parseQueryString('https://scriptoj.com/problems/#?offset=10&limit=100'))
+//   }
+//   console.log(parseQueryString('  https://scriptoj.com/problems/?offset=&limit=100#name=jerry '));
+//   //console.log(parseQueryString('https://scriptoj.com/problems/#?offset=10&limit=100'))
 
-// // let  time = '2014-04-23'
-// // let date  = new Date(time);
-// // console.log(Date.parse(time))
-// // console.log(Date.parse(date))
-// // console.log(date.valueOf())
-// // console.log(date.getTime())
-// // String.prototype.add = function(){
-// //     console.log(this)
+// let  time = '2014-04-23'
+// let date  = new Date(time);
+// console.log(Date.parse(time))
+// console.log(Date.parse(date))
+// console.log(date.valueOf())
+// console.log(date.getTime())
+// String.prototype.add = function(){
+//     console.log(this)
 //     console.log(this+'2')
    
-// // }
+// }
 
 // class EventEmitter {
 //     constructor() {
@@ -109,34 +109,43 @@
 //console.log(new Date(1111111))
 
 
-var isAnagram = function(s, t) {
-    let obj = {};
-    let o= {};
-    if(s.length != t.length) return false;
-    let st = s.split('');
-    let tt = t.split('');
-    st.forEach((val)=>{
-        if(obj[val]){
-            ++obj[val] ;
-        }else {
-            obj[val] = 1;
-        }
-    });
-    tt.forEach((val)=>{
-        if(o[val]){
-            ++o[val];
-        }else {
-            o[val] = 1;
-        }
-    });
+// var isAnagram = function(s, t) {
+//     let obj = {};
+//     let o= {};
+//     if(s.length != t.length) return false;
+//     let st = s.split('');
+//     let tt = t.split('');
+//     st.forEach((val)=>{
+//         if(obj[val]){
+//             ++obj[val] ;
+//         }else {
+//             obj[val] = 1;
+//         }
+//     });
+//     tt.forEach((val)=>{
+//         if(o[val]){
+//             ++o[val];
+//         }else {
+//             o[val] = 1;
+//         }
+//     });
    
-    for(let i in obj) {
-        if(!o[i] ||  o[i] != obj[i]){
-            return false;
-        }
-    }
-    return true;
-};
+//     for(let i in obj) {
+//         if(!o[i] ||  o[i] != obj[i]){
+//             return false;
+//         }
+//     }
+//     return true;
+// };
 
-let b = isAnagram("anagram","nagaram");
-console.log(b)
+// let b = isAnagram("anagram","nagaram");
+// console.log(b)
+
+function grid() {
+    +function() {
+       var i = 1;
+    }();
+    
+   console.log(i)
+}
+grid();
