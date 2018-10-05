db750e625bcf677c4eac7def36fd1fb80cdf7f88
fengzi2016 <1820166258@qq.com>
2018-10-05 22:11:21 +0800

    feat:add new

---
+++ javascript/questionThinking/sum.valueOf.md
@@ -0,0 +1,23 @@
+问题：实现一个函数sum(1,2)(3).valueOf() => 6
+
+思路：
+> 首先这是个函数，调用了valueOf方法。每个JS对象都有内置的valueOf方法，不同的对象调用的原理不一样，这个方法可以被重写。Obejct,Number和函数是先调用valueOf，如果没有返回字符型结果就调用toString方法，如果还是没有返回字符型结果则为undefined；String是先调用toString再调用valueOf,直到返回字符型结果为止，否则为Undefined
+> 所以题目函数的valueOf应该被重写了，返回了一个字符型结果，即总和
+> sum可以不停的调用，说明是返回了一个函数
+> 最后的和是所有参数的和，说明主函数内有变量对参数进行了积累（递归积累）,
+
+```js
+    function sum() {
+        let args = Array.prototype.slice.call(arguments); // 积累参数
+        function help() {
+            let _args = Array.prototype.slice.call(arguments);
+            return sum.apply(null,args.concat(_args)); // 不断递归
+        }
+        help.valueOf = function() {
+            return args.reduce((a,b)=>{
+                return a+b;
+            },0)
+        }
+        return help;
+    }
+```
--- javascript/questionThinking/toChineseNumber.js
+++ javascript/questionThinking/toChineseNumber.js
@@ -6,23 +6,33 @@
 // n万0十n
 // n万0n
 
-const toChineseNum = (num) => {
-    let arr = num.toString().split("");
-    let per = ['万','千','百','十'];
-    let china = ['零','一','二','三','四','五','六','七','八','九'];
-    let start = per.length+1-arr.length;
-    let result = "";
-    for(let i=0;i<arr.length;i++) {
+// const toChineseNum = (num) => {
+//     let arr = num.toString().split("");
+//     let per = ['万','千','百','十'];
+//     let china = ['零','一','二','三','四','五','六','七','八','九'];
+//     let start = per.length+1-arr.length;
+//     let result = "";
+//     for(let i=0;i<arr.length;i++) {
         
-      result+=china.find((w,index)=>{
-        return Number(arr[i])===index;
-      });
+//       result+=china.find((w,index)=>{
+//         return Number(arr[i])===index;
+//       });
       
-      result+=per[start]&&arr[i]!=='0'?per[start]:"";
-      start++;
-    }
-    result=result.replace(/(零*$)/,"")
-    result=result.replace(/(零+)/,"零")
-    return result;
+//       result+=per[start]&&arr[i]!=='0'?per[start]:"";
+//       start++;
+//     }
+//     result=result.replace(/(零*$)/,"")
+//     result=result.replace(/(零+)/,"零")
+//     return result;
+//   }
+// console.log(toChineseNum(66046247));
+function Vue() {
+  this._init('ss')
+}
+function initMixin(Vue) {
+  Vue.prototype._init = function (options) {
+    console.log(this)
   }
-console.log(toChineseNum(66046247));
+}
+initMixin(Vue);
+Vue()
