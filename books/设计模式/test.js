f3e4efc5ee09da964d783760399cfb6e1f154125
guanyunfeng <1820166258@qq.com>
2019-05-27 11:53:08 +0800

    适配器模式

--- books/设计模式/test.js
+++ books/设计模式/test.js
@@ -1,26 +1,26 @@
 // 1.  window.external.upload 函数，在页面中模拟创建上传插件
 window.external.upload = function(state) {
   console.log(state);
-}
+};
 var plugin = (function() {
-  var plugin = document.createElement('embed');
-  plugin.style.display = 'none';
-  plugin.type = 'application/txftn-webkit';
+  var plugin = document.createElement("embed");
+  plugin.style.display = "none";
+  plugin.type = "application/txftn-webkit";
   plugin.sign = function() {
-    console.log('开始文件扫描');
+    console.log("开始文件扫描");
   };
   plugin.pause = function() {
-    console.log('暂停文件上传');
+    console.log("暂停文件上传");
   };
   plugin.uploading = function() {
-    console.log('开始文件上传');
+    console.log("开始文件上传");
   };
   plugin.del = function() {
-    console.log('删除文件上传');
+    console.log("删除文件上传");
   };
   plugin.done = function() {
-    console.log('文件上传完成');
-  }
+    console.log("文件上传完成");
+  };
   document.body.appendChild(plugin);
   return plugin;
 })();
@@ -36,119 +36,195 @@ var Upload = function(fileName) {
   this.doneState = new DoneState(this);
   this.errorState = new ErrorState(this);
   this.currState = this.signState;
-}
+};
 // 负责往页面中创建跟上传流程有关的DOM 节点，并开始绑定按钮的事件：
 Upload.prototype.init = function() {
-  this.dom = document.createElement('div');
-  this.dom.innerHTML =  '<span>文件名称:'+ this.fileName +'</span>\
+  this.dom = document.createElement("div");
+  this.dom.innerHTML =
+    "<span>文件名称:" +
+    this.fileName +
+    '</span>\
   <button data-action="button1">扫描中</button>\
   <button data-action="button2">删除</button>';
   document.body.appendChild(this.dom);
   this.button1 = this.dom.querySelector('[data-action="button1"]');
   this.button2 = this.dom.querySelector('[data-action="button2"]');
   this.bindEvent();
-}
+};
 // 负责具体的按钮事件实现，在点击了按钮之后，Context 并不做任何具体的操作，而是把请求委托给当前的状态类来执行：
 Upload.prototype.bindEvent = function() {
   var self = this;
   this.button1.onclick = function() {
     self.currState.clickHandler1();
-  }
+  };
   this.button2.onclick = function() {
     self.currState.clickHandler2();
-  }
+  };
 };
 Upload.prototype.sign = function() {
   this.plugin.sign();
   this.currState = this.signState;
-}
-Upload.prototype.uploading = function(){
-  this.button1.innerHTML = '正在上传，点击暂停';
+};
+Upload.prototype.uploading = function() {
+  this.button1.innerHTML = "正在上传，点击暂停";
   this.plugin.uploading();
   this.currState = this.uploadingState;
- };
- Upload.prototype.pause = function(){ 
-  this.button1.innerHTML = '已暂停，点击继续上传';
+};
+Upload.prototype.pause = function() {
+  this.button1.innerHTML = "已暂停，点击继续上传";
   this.plugin.pause();
   this.currState = this.pauseState;
- };
- Upload.prototype.done = function(){
-  this.button1.innerHTML = '上传完成';
+};
+Upload.prototype.done = function() {
+  this.button1.innerHTML = "上传完成";
   this.plugin.done();
   this.currState = this.doneState;
- };
- Upload.prototype.error = function(){
-  this.button1.innerHTML = '上传失败';
+};
+Upload.prototype.error = function() {
+  this.button1.innerHTML = "上传失败";
   this.currState = this.errorState;
- };
- Upload.prototype.del = function(){
+};
+Upload.prototype.del = function() {
   this.plugin.del();
-  this.dom.parentNode.removeChild( this.dom );
- }; 
- // 各个状态类的实现
- var StateFactory = (function() {
-   var State = function(){};
-   State.prototype.clickHandler1 = function(){
-     throw new Error('子类必须重写父类的 clickHandler1 方法')
-   }
-   State.prototype.clickHandler2 = function() {
-    throw new Error( '子类必须重写父类的 clickHandler2 方法' );
-   }
-   return function(param) {
-     var F = function(uploadObj) {
-       this.uploadObj = uploadObj;
-     }
-     F.prototype = new State();
-     for( var i in param) {
-       F.prototype[i] = param[i];
-     }
-     return F;
-   }
- })()
-
- var SignState = StateFactory({
-    clickHandler1: function(){
-     console.log('扫描中，点击无效...')
-    },
-    clickHandler2: function(){
-     console.log( '文件正在上传中，不能删除' );
+  this.dom.parentNode.removeChild(this.dom);
+};
+// 各个状态类的实现
+var StateFactory = (function() {
+  var State = function() {};
+  State.prototype.clickHandler1 = function() {
+    throw new Error("子类必须重写父类的 clickHandler1 方法");
+  };
+  State.prototype.clickHandler2 = function() {
+    throw new Error("子类必须重写父类的 clickHandler2 方法");
+  };
+  return function(param) {
+    var F = function(uploadObj) {
+      this.uploadObj = uploadObj;
+    };
+    F.prototype = new State();
+    for (var i in param) {
+      F.prototype[i] = param[i];
     }
- })
-;
+    return F;
+  };
+})();
+
+var SignState = StateFactory({
+  clickHandler1: function() {
+    console.log("扫描中，点击无效...");
+  },
+  clickHandler2: function() {
+    console.log("文件正在上传中，不能删除");
+  }
+});
 var UploadingState = StateFactory({
-  clickHandler1: function(){
-  this.uploadObj.pause();
+  clickHandler1: function() {
+    this.uploadObj.pause();
   },
-  clickHandler2: function(){
-  console.log( '文件正在上传中，不能删除' );
+  clickHandler2: function() {
+    console.log("文件正在上传中，不能删除");
   }
- });
- var PauseState = StateFactory({
-  clickHandler1: function(){
-  this.uploadObj.uploading();
+});
+var PauseState = StateFactory({
+  clickHandler1: function() {
+    this.uploadObj.uploading();
   },
-  clickHandler2: function(){
-  this.uploadObj.del();
+  clickHandler2: function() {
+    this.uploadObj.del();
   }
- }); 
- var ErrorState = StateFactory({
-  clickHandler1: function(){
-  console.log( '文件上传失败, 点击无效' );
+});
+var ErrorState = StateFactory({
+  clickHandler1: function() {
+    console.log("文件上传失败, 点击无效");
   },
-  clickHandler2: function(){
-  this.uploadObj.del();
+  clickHandler2: function() {
+    this.uploadObj.del();
   }
 });
 // 测试
-var uploadObj = new Upload( 'JavaScript 设计模式与开发实践' );
+var uploadObj = new Upload("JavaScript 设计模式与开发实践");
 uploadObj.init();
-window.external.upload = function( state ){
- uploadObj[ state ]();
+window.external.upload = function(state) {
+  uploadObj[state]();
 };
-window.external.upload( 'sign' ); 
-setTimeout(function(){
- window.external.upload( 'uploading' ); // 1 秒后开始上传
-}, 1000 );
-setTimeout(function(){
- window.external.upload( 'done' ); // 5 秒后上传完成
-}, 5000 );
+window.external.upload("sign");
+setTimeout(function() {
+  window.external.upload("uploading"); // 1 秒后开始上传
+}, 1000);
+setTimeout(function() {
+  window.external.upload("done"); // 5 秒后上传完成
+}, 5000);
+// 方法一
+var Light = function() {
+  this.currState = FSM.off; // 设置当前状态
+  this.button = null;
+};
+Light.prototype.init = function() {
+  var button = document.createElement("button"),
+    self = this;
+  button.innerHTML = "已关灯";
+  this.button = document.body.appendChild(button);
+  this.button.onclick = function() {
+    self.currState.buttonWasPressed.call(self); // 把请求委托给 FSM 状态机
+  };
+};
+var FSM = {
+  off: {
+    buttonWasPressed: function() {
+      console.log("关灯");
+      this.button.innerHTML = "下一次按我是开灯";
+      this.currState = FSM.on;
+    }
+  },
+  on: {
+    buttonWasPressed: function() {
+      console.log("开灯");
+      this.button.innerHTML = "下一次按我是关灯";
+      this.currState = FSM.off;
+    }
+  }
+};
+var light = new Light();
+light.init();
+// 方法二
+var delegate = function(client, delegation) {
+  return {
+    buttonWasPressed: function() {
+      // 将客户的操作委托给 delegation 对象
+      return delegation.buttonWasPressed.apply(client, arguments);
+    }
+  };
+};
+var FSM = {
+  off: {
+    buttonWasPressed: function() {
+      console.log("关灯");
+      this.button.innerHTML = "下一次按我是开灯";
+      this.currState = this.onState;
+    }
+  },
+  on: {
+    buttonWasPressed: function() {
+      console.log("开灯");
+      this.button.innerHTML = "下一次按我是关灯";
+      this.currState = this.offState;
+    }
+  }
+};
+var Light = function() {
+  this.offState = delegate(this, FSM.off);
+  this.onState = delegate(this, FSM.on);
+  this.currState = this.offState; // 设置初始状态为关闭状态
+  this.button = null;
+};
+Light.prototype.init = function() {
+  var button = document.createElement("button"),
+    self = this;
+  button.innerHTML = "已关灯";
+  this.button = document.body.appendChild(button);
+  this.button.onclick = function() {
+    self.currState.buttonWasPressed();
+  };
+};
+var light = new Light();
+light.init();
