60b87649a8a1509d0ef7bd6581047d19920e3c7d
fengzi2016 <1820166258@qq.com>
2019-08-26 22:18:43 +0800

    feat: 添加前端模块化

--- share/javascript/module.js
+++ share/javascript/module.js
@@ -1,1 +1,45 @@
-console.log('a')
+
+  var token = [];
+  const start = char => {
+
+      if(typeof char === 'symbol') return start;
+      if(/[0-9]/.test(char))
+       {
+          token.push(char);
+          return inNumber;   
+      }
+      if(/[\+\-\*\/]/.test(char)
+      ) {
+          emmitToken("Char", char);
+          return start
+      }
+      if(char === ' ' || char === '\r' 
+      || char === '\n') {
+          return start;
+      }
+      
+  }
+  const inNumber = char => {
+      if(/[0-9]/.test(char)) {
+          token.push(char);
+          return inNumber;
+      } else {
+          emmitToken("Number", token.join(""));
+          token = [];
+          return start(char); // put back char
+      }
+  }
+  
+function emmitToken(type, value) {
+    console.log(value);
+}
+
+var input = "1024 + 2 * 256"
+
+var state = start;
+
+for(var c of input.split(''))
+    state = state(c);
+
+state('EOF')
+
